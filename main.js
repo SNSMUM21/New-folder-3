a68b328482eeeb3fd288347d88f044f783861fbb
SNSMUM21 <82374302+SNSMUM21@users.noreply.github.com>
2021-11-09 12:04:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot()
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/QWOVuh3lt/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/w7Pgxs5Vu/model.json',modelLoaded);
 
 function modelLoaded() {
 console.log('Model Loaded!');
